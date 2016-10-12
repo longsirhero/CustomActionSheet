391296c968625cd6e1ca670f79c0716e1286cc4c
longsirhero <wingching@wingching-yipdeimac.local>
2016-10-12 16:30:43 +0800

    Initial Commit

---
+++ CustomActionSheet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomActionSheet
+//
+//  Created by WingChing_Yip on 16/10/12.
+//  Copyright © 2016年 Dinpay Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomActionSheet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomActionSheet
+//
+//  Created by WingChing_Yip on 16/10/12.
+//  Copyright © 2016年 Dinpay Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
